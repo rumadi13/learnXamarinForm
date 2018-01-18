961ddd6019d02300197a157170ce138755fd2f97
rumadi13 <rumadi13@gmail.com>
2018-01-18 16:19:48 +0700

    content Page

--- BasicAndroid/BasicAndroid/BasicAndroid/App.xaml.cs
+++ BasicAndroid/BasicAndroid/BasicAndroid/App.xaml.cs
@@ -13,22 +13,22 @@ namespace BasicAndroid
 		{
 			InitializeComponent();
 
-			MainPage = new BasicAndroid.MainPage();
+			MainPage = new HomePage();
 		}
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+		//protected override void OnStart ()
+		//{
+		//	// Handle when your app starts
+		//}
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+		//protected override void OnSleep ()
+		//{
+		//	// Handle when your app sleeps
+		//}
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
+		//protected override void OnResume ()
+		//{
+		//	// Handle when your app resumes
+		//}
 	}
 }
