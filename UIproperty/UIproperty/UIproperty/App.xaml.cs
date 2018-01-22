24f6245695756a6a81ec176ed5d2d902c786e22b
rumadi13 <rumadi13@gmail.com>
2018-01-22 13:33:07 +0700

    UI properti demo

--- UIproperty/UIproperty/UIproperty/App.xaml.cs
+++ UIproperty/UIproperty/UIproperty/App.xaml.cs
@@ -13,22 +13,22 @@ namespace UIproperty
 		{
 			InitializeComponent();
 
-			MainPage = new UIproperty.MainPage();
+			MainPage = new pageUI();
 		}
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+		//protected override void OnStart ()
+		//{
+		//	// Handle when your app starts
+		//}
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+		//protected override void OnSleep ()
+		//{
+		//	// Handle when your app sleeps
+		//}
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
+		//protected override void OnResume ()
+		//{
+		//	// Handle when your app resumes
+		//}
 	}
 }
