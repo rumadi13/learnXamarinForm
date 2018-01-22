052ece754adeab385af7ee3bd096117db6373f85
rumadi13 <rumadi13@gmail.com>
2018-01-22 11:46:42 +0700

    Content Page

---
+++ label/label/label/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace label
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        public void button_LabelonClicked()
+        {
+            var text = MainEntry.Text;
+            MainLabel.Text = "Hello " + text + "Ganteng";
+        }
+    }
+}
